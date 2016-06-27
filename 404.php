53195d206e49a64c27fa251eb08460679cc78aa0
vasin limsukhawat <vasin.limsukhawat@gmail.com>
2016-06-26 20:46:21 -0600

    added 404 and single

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
   <?php get_header(); ?>
   <div class="col col-sm-10">
     <article class="post">
-      <h1>No content found</h1>
+      <h1>404 Page Not Found</h1>
     </article>
   </div>
 
--- single.php
+++ single.php
@@ -1,25 +1,18 @@
+<?php get_header(); ?>
 <?php
-
-get_header();
-
-if (have_posts()) :
-  while (have_posts()) : the_post(); ?>
-<div class="col col-sm-10">
-  <article class="post">
-    <h1 class="post-title "><?php the_title(); ?></h1>
-    <div class="post-content">
-      <?php the_content(); ?>
-    </div>
-  </article>
-</div>
-
-<?php endwhile;
-
+  if (have_posts()) :
+    while (have_posts()) : the_post(); ?>
+  <div class="col col-sm-10">
+    <article class="post">
+      <h1 class="post-title "><?php the_title(); ?></h1>
+      <div class="post-content">
+        <?php the_content(); ?>
+      </div>
+    </article>
+  </div>
+  <?php endwhile;
   else :
     echo '<p>No content found </p>';
-
   endif;
-
-
-get_footer();
 ?>
+<?php get_footer(); ?>
